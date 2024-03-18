c0c947eb7e7e275daf79751db0e60453d7452694
Sergey-Smirnov-redshelf <sergeysmirnov@coherentsolutions.com>
2024-03-18 18:26:39 +0300

    Update github checks and docker-compose

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-s3g$+uudq6&$ox2@01op$42wy!8shqdl8s%=+1$r&w-qfb^@5^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
